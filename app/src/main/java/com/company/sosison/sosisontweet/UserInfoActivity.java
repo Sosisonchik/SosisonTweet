7d69b7346514d1df106fc3bcf861b43e902bc5e6
Andrii Marusik <child4240@gmail.com>
2018-03-17 14:49:51 +0200

    tool bar was last

--- app/src/main/java/com/company/sosison/sosisontweet/UserInfoActivity.java
+++ app/src/main/java/com/company/sosison/sosisontweet/UserInfoActivity.java
@@ -2,12 +2,17 @@ package com.company.sosison.sosisontweet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class UserInfoActivity extends AppCompatActivity {
-
+    ImageView userImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
+        userImage = (ImageView)findViewById(R.id.user_image_view);
+        Picasso.with(this).load("http://i.imgur.com/DvpvklR.png").into(userImage);
     }
 }
